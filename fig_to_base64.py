e67d214d3b57ac33b6b09eee31363253fc25f8ff
Greyyy-HJC <hejinchen17@mails.ucas.ac.cn>
2021-04-15 12:34:10 +0800

    self_renormolizaion

---
+++ fig_to_base64.py
@@ -0,0 +1,14 @@
+# %%
+import base64
+
+f=open('1.png','rb') #二进制方式打开图文件
+ls_f=base64.b64encode(f.read()) #读取文件内容，转换为base64编码
+f.close()
+print(ls_f)
+
+# %%
+bs='iVBORw0KGgoAAAANSUhEUg....' # 太长了省略
+imgdata=base64.b64decode(bs)
+file=open('2.jpg','wb')
+file.write(imgdata)
+file.close()
