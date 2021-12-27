4dcdfece82dd9a035c26e84ef6580829c5f5dfb2
Greyyy-HJC <hejinchen17@gmail.com>
2021-12-27 14:06:23 +0800

    upload_notes

--- fig_to_base64/fig_to_base64.py
+++ fig_to_base64/fig_to_base64.py
@@ -1,14 +1,15 @@
 # %%
 import base64
+import gvar as gv
 
-f=open('1.png','rb') #二进制方式打开图文件
+f=open('SM.png','rb') #二进制方式打开图文件
 ls_f=base64.b64encode(f.read()) #读取文件内容，转换为base64编码
 f.close()
-print(ls_f)
+gv.dump(ls_f, 'ls_f')
 
 # %%
-bs='iVBORw0KGgoAAAANSUhEUg....' # 太长了省略
+bs=gv.load('ls_f')
 imgdata=base64.b64decode(bs)
-file=open('2.jpg','wb')
+file=open('SM_re.jpg','wb')
 file.write(imgdata)
 file.close()
